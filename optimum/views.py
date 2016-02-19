76dc33cb420c460f00c979daa67f2bf3f65dd3a2
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2016-02-19 18:13:43 +0200

    Change  path of credentials file.

--- optimum/views.py
+++ optimum/views.py
@@ -155,7 +155,7 @@ def get_file_content(service, file_id):
 
 def get_service(request):
 
-        flow = flow_from_clientsecrets('optimum\static\json\json_credentials.json',
+        flow = flow_from_clientsecrets('/root/optimum/optimum/static/json/json_credentials.json',
                                scope='https://www.googleapis.com/auth/drive',
                                redirect_uri='http://snf-697531.vm.okeanos.grnet.gr:8088/')
 
